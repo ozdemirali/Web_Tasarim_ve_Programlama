20d7d1ae26f06b3fd3acfed5fcbbf685d8af3ae4
Ali <ozdemirali@hotmail.com>
2021-09-16 13:53:44 +0300

    Web Site is done

---
+++ 2.Hafta/Academy/script/promo.js
@@ -0,0 +1,8 @@
+//console.log("Parogram çalıştı");
+
+//Bu bir fonksiyon. Bu yanlızca tetiklenince yada çağrılınca çalışır.
+function closePromo(){
+    //alert("Çalışrı");
+    // console.log("Kapat fonksiyonu çalıştı");
+     document.getElementById("promo").style.display="none";
+}
