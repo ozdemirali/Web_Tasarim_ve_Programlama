3e5edd466f7655a238483c0d50a18b084eee9fcd
Ali Özdemir <ozdemirali@hotmail.com>
2021-09-23 22:31:44 +0300

    Add files via upload

--- 2.Hafta/Academy/script/promo.js
+++ 2.Hafta/Academy/script/promo.js
@@ -1,3 +1,10 @@
+
+document.addEventListener('scroll', function(e) {
+    //console.log(window.scrollY);
+    if(window.scrollY>0){
+        window.scrollTo(0,0);
+    }
+  });
 //console.log("Parogram çalıştı");
 
 //Bu bir fonksiyon. Bu yanlızca tetiklenince yada çağrılınca çalışır.
@@ -5,4 +12,13 @@ function closePromo(){
     //alert("Çalışrı");
     // console.log("Kapat fonksiyonu çalıştı");
      document.getElementById("promo").style.display="none";
+}
+
+function changeColor(){
+    var r=Math.floor(Math.random() * 255);
+    var g=Math.floor(Math.random() * 255);
+    var b=Math.floor(Math.random() * 255);
+    
+    var renk="rgb("+r+","+g+","+b+")";
+    document.getElementById("contentHead").style.color=renk;
 }
