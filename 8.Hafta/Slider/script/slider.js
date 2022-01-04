7874f100013bcfcb0f849b4016e5d99552b5f48a
Ali Özdemir <ozdemirali@hotmail.com>
2022-01-04 23:20:49 +0300

    add

---
+++ 8.Hafta/Slider/script/slider.js
@@ -0,0 +1,12 @@
+function change_image(resim){
+   console.log(resim); 
+   document.getElementById("img").src=resim;
+//    document.getElementById("img").src="img/slider 1.jpeg";
+//    document.getElementById("img").src="img/slider 2.jpeg";
+
+}
+
+// function change_image2(){
+//     console.log("2 çalıştı"); 
+//     document.getElementById("img").src="img/slider 2.jpeg";
+//  }
